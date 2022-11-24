f387476f3921346b8422f61bfca59b1f69413c00
Chung SungWoong <dnd8888@naver.com>
2022-11-24 12:26:13 +0900

    Divide the train set and test set

--- MultiClassification2.py
+++ MultiClassification2.py
@@ -2,6 +2,7 @@ from sklearn.preprocessing import LabelEncoder
 from keras.utils import np_utils
 from keras.models import Sequential
 from keras.layers.core import Dense
+from sklearn.model_selection import train_test_split
 
 import pandas as pd
 import seaborn as sns
@@ -13,8 +14,7 @@ seed = 0
 numpy.random.seed(seed)
 tf.random.set_seed(seed)
 
-#df = pd.read_csv('/Users/chung_sungwoong/Desktop/Practice/DeepLearning_Practice/dataset/iris.csv', names = ['special_length','sepal_width','petal_length','petal_width','species'])
-df = pd.read_excel('/Users/chung_sungwoong/Desktop/Check2.xlsx', names = ['accuracy','time','user'])
+df = pd.read_excel('/Users/chung_sungwoong/Desktop/Check.xlsx', names = ['accuracy','time','user'])
 
 dataset = df.values
 x = dataset[:,0:2].astype(float)
@@ -25,13 +25,16 @@ e.fit(Y_obj)
 Y = e.transform(Y_obj)
 Y_encoded = np_utils.to_categorical(Y)      #원 핫 인코딩
 
+x_train, x_test, y_train,y_test = train_test_split(x,Y_encoded,test_size = 0.3, random_state = seed)
+
 #소프트맥스 모델
 
 model = Sequential()
 model.add(Dense(16,input_dim=2,activation='relu'))
-model.add(Dense(3,activation='softmax'))
+model.add(Dense(3,activation='sigmoid'))
+
 
 model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-model.fit(x,Y_encoded,epochs=50, batch_size=1)
+model.fit(x_train,y_train,epochs=50, batch_size=1)
 
-print("\n Accuracy: %.4f" %(model.evaluate(x,Y_encoded)[1]))
+print("\n Accuracy: %.4f" %(model.evaluate(x_test,y_test)[1]))
