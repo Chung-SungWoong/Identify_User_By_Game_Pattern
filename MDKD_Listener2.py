4264cec256e4bcfa7c9986194e938c548f280d5d
Chung SungWoong <dnd8888@naver.com>
2022-12-02 19:41:48 +0900

    Get the mouse position with time

--- MDKD_Listener2.py
+++ MDKD_Listener2.py
@@ -1,3 +1,4 @@
+"""
 from pynput.keyboard import Listener  as KeyboardListener
 from pynput.mouse    import Listener  as MouseListener
 from pynput.keyboard import Key
@@ -22,27 +23,35 @@ def on_click(x, y, button, pressed):
 def on_scroll(x, y, dx, dy):
     logging.info('Mouse scrolled at ({0}, {1})({2}, {3})'.format(x, y, dx, dy))
 
-with MouseListener(on_click=on_click, on_move=on_move) as listener:
+with MouseListener(on_click=on_click) as listener:
     with KeyboardListener(on_press=on_press) as listener:
         listener.join()
-        time.sleep(0.2)
-
-
 """
+
 from pynput.mouse import Controller as mouse_control
+from pynput.mouse import Listener as MouseListener
+from pynput.keyboard import Listener  as KeyboardListener
+
 from pynput import keyboard as key_control
+import time
 
 mouse = mouse_control() #mouse controller
 
 def on_press (key) :
-    print(key.char)
+    print(key)
 
-listener = key_control.Listener(on_press=on_press) #keyboard listener
+def on_click(x, y, button, pressed):
+    if pressed:
+        print('Mouse clicked at ({0}, {1}) with {2}'.format(x, y, button))
+
+with MouseListener(on_click=on_click) as listener:
+    with KeyboardListener(on_press=on_press) as listener:
+        listener.join()
+
+listener = key_control.Listener(on_press=on_press, on_click = on_click) #keyboard listener
 listener.start()
 
 while True :
     command = mouse.position
     print(command)
     time.sleep(0.2)
-
-"""
