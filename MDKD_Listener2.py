bb5eae5d3b7937f7f02f4d1d0e5813fba1d21b72
Chung SungWoong <dnd8888@naver.com>
2022-12-13 22:11:49 +0900

    Add timestamp function

--- MDKD_Listener2.py
+++ MDKD_Listener2.py
@@ -1,32 +1,13 @@
 # Import the pynput library and time module
 from pynput.mouse import Listener as MouseListener
 from pynput.keyboard import Listener as KeyboardListener
+from datetime import datetime
 import pyautogui
 import time
 global last_x, last_y, last_t
-# Define the functions for the mouse and keyboard listeners
-"""
-우선 on_move 를 없에고 안의 내용을 밑의 while: 아래에 넣어야 할 것 같음
 
-def on_move(x, y):
-    global last_x, last_y, last_t
-    # Calculate the time since the last movement
-    t = time.perf_counter()
-    dt = t - last_t
-    last_t = t
-    
-    # Calculate the distance moved since the last movement
-    dx = x - last_x
-    dy = y - last_y
-    last_x = x
-    last_y = y
-    
-    # Calculate the velocity and acceleration
-    velocity = (dx**2 + dy**2)**0.5 / dt
-    acceleration = velocity / dt
-    
-    print('{0},velocity {1:.2f}, acceleration {2:.2f}'.format((x, y), velocity, acceleration))
-"""
+# Define the functions for the mouse and keyboard listeners
+
 def on_click(x, y,button, pressed):
     print('{0} at {1}'.format('Pressed' if pressed else 'Released', (x, y)))
 
@@ -45,10 +26,11 @@ with MouseListener(on_click=on_click) as mouse_listener:
         
         # Get the initial position of the mouse
         last_x, last_y = pyautogui.position()
-        
-        # Log the mouse position every 0.2 seconds
+
+        # Log the mouse position every 0.5 seconds
         while True:
             time.sleep(0.5)
+            s = datetime.fromtimestamp(time.time())
             t = time.perf_counter()
             x, y = pyautogui.position()
             dt = t - last_t
@@ -63,4 +45,4 @@ with MouseListener(on_click=on_click) as mouse_listener:
             # Calculate the velocity and acceleration
             velocity = (dx**2 + dy**2)**0.5 / dt
             acceleration = velocity / dt
-            print('{0},velocity {1:.2f}, acceleration {2:.2f}'.format((x, y), velocity, acceleration))
+            print('{0},velocity {1:.2f}, acceleration {2:.2f}, Date:{3}'.format((x, y), velocity, acceleration, s))
